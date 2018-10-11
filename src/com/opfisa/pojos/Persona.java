11f23451dfc274c598c592ba7137fcad1f35dac6
Jonatan Vera <jonatan.e.vera@gmail.com>
2018-10-11 19:01:54 -0300

    Se agregan los package gui y pojos

--- src/com/opfisa/pojos/Persona.java
+++ src/com/opfisa/pojos/Persona.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ofi.base;
+package com.opfisa.pojos;
 
 /**
  *
- * @author CONDMD29
+ * @author ACER
  */
-public class BaseDatos {
+public class Persona {
     
 }
