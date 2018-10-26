31ab38540205911a92d3ba106b7ccb0f72fba5d7
jvera <jonatan.e.vera@gmail.com>
2018-10-26 11:04:33 -0300

    Se agregan ventanas de gestión y la clase que va a administrar la conexión a los DBF's

--- src/com/opfisa/base/BaseDatos.java
+++ src/com/opfisa/base/BaseDatos.java
@@ -5,10 +5,36 @@
  */
 package com.opfisa.base;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author CONDMD29
  */
 public class BaseDatos {
+    
+    public static Connection conectar(){
+		Connection con = null;
+		
+		String DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
+		String URL = "jdbc:odbc:Personas";
+		String USER = "";
+		String PASSWD = "";
+		
+		try {
+			con = DriverManager.getConnection(URL, USER, PASSWD);
+			if(con != null)
+				System.out.println("Conectado");
+			
+		} catch (SQLException e) {
+			System.out.println("No se pudo conectar");
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
     
 }
